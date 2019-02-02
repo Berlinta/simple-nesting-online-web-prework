a348feef5b485a5beaa0ba90c86f9dec24b095f6
Nash Brancic <nashfft@gmail.com>
2019-02-02 05:05:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-return [:grace_hopper]
+return {:known_for=>"COBOL", :languages=>["COBOL", "FORTRAN"]}
 end
 
 def alan_kay_is_known_for
