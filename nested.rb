e23aac8c0edd0a34d6eedf89a084e9abf47ff001
Nash Brancic <nashfft@gmail.com>
2019-02-02 05:21:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     changing_alan[:known_for] = "GUI"
+     :alan_kay[:known_for] = "GUI"
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
